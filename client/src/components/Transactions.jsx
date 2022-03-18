9d7222ac24efb3efe1ff99b7366beac8c819c9ec
Nikita <nvadeveloper@gmail.com>
2022-03-18 17:19:39 +0300

    add giphy

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from "react";
 
 import { TransactionContext } from "../context/TransactionContext"
+import useFetch from "../hooks/useFetch";
 import dummyData from "../utils/dummyData";
 
 import { shortenAddress } from '../utils/shortenAddress';
 
+
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-  return 
+
+  const gifUrl = useFetch({ keyword });
+
+  return (
     <div className='bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
@@ -15,12 +20,33 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
       flex-col p-3 rounded-md hover:shadow-2xl
     '>   
       <div className='flex flex-col items-center w-full mt-3'>
-        <div className='flex justify-start w-full mb-6 p-2'>
-          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`}></a>
+        <div className='w-full mb-6 p-2'>
+          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target='_blank' rel="noopener noreferrer">
+            <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+          </a>
+          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target='_blank' rel="noopener noreferrer">
+            <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+          </a>
+          <p className="text-white text-base">Amount: {amount} ETH</p>
+          {message && (
+            <>
+              <br />
+              <p className="text-white text-base">Message: {message}</p>
+            </>
+          )}
 
+          <img 
+            src={gifUrl || url}
+            alt="gif"
+            className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
+          />
+        </div>
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className="text-[#37c7da] font-bold ">{timestamp}</p>
         </div>
       </div>
     </div>
+  ) 
 }
 
 const Transactions = () => {
